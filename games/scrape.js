719c9aee2d81c296beea37ae397b88594982cd77
NDIIESC <105543597+NDIIESC@users.noreply.github.com>
2023-09-15 11:57:22 +0800

    Add files via upload

---
+++ games/scrape.js
@@ -0,0 +1,10 @@
+[ 
+   { 
+     "posisi": "Kiper",
+     "negara": "🏴󠁧󠁢󠁥󠁮󠁧󠁿",
+     "liga": "Liga Inggris",
+     "klub": "Arsenal",
+     "nopung": "1",
+     "jawaban": "Aaron Ramsdale"
+   }, 
+]
